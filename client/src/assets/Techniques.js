6428ae7a9cd2a54f8fae6e85bb5329cb69b84467
William Brown <willis291@gmail.com>
2023-10-18 15:01:34 -0600

    Initial formatting of technique cards complete

--- client/src/assets/Techniques.js
+++ client/src/assets/Techniques.js
@@ -1,97 +1,118 @@
 //This will be the list of information that will be used for the technique cards
+// Each of the guidelines is defined here as a key within the Techniques object. Each key contains an array of objects that contain the info that shall be rendered on the guideline page
+import placeholder from "../assets/images/techniques/placeholder.jpg";
 const Techniques = {
   guideline1Techniques: [
     {
       title: "Guideline 1 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 1 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 1 technique 3",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline2Techniques: [
     {
       title: "Guideline 2 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 2 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline3Techniques: [
     {
       title: "Guideline 3 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 3 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline4Techniques: [
     {
       title: "Guideline 4 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 4 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline5Techniques: [
     {
       title: "Guideline 5 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 5 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline6Techniques: [
     {
       title: "Guideline 6 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 6 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline7Techniques: [
     {
       title: "Guideline 7 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 7 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline8Techniques: [
     {
       title: "Guideline 8 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 8 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
   guideline9Techniques: [
     {
       title: "Guideline 9 technique 1",
       description: "This is a description of the technique",
+      image: placeholder,
     },
     {
       title: "Guideline 9 technique 2",
       description: "This is a description of the technique",
+      image: placeholder,
     },
   ],
 };
--- client/src/components/techniqueCard/TechniqueCard.js
+++ client/src/components/techniqueCard/TechniqueCard.js
@@ -2,11 +2,15 @@
 
 import React from "react";
 import Techniques from "../../assets/Techniques";
+import { Grid } from "@mui/material";
+import styles from "./TechniqueCard.module.css";
+import { styled } from "@mui/material/styles";
 
 function TechniqueCard({ guidelineInt }) {
   console.log(`The current guideline int is ${guidelineInt}`);
   var currTechniqueList = null;
   //This switch statement will determine which list of techniques to use based on the guidelineInt
+  //The techniques within the list will then be rendered on the page appropriately
   switch (guidelineInt) {
     case "glvc1":
       currTechniqueList = Techniques.guideline1Techniques;
@@ -38,13 +42,31 @@ function TechniqueCard({ guidelineInt }) {
     default:
       currTechniqueList = null;
   }
+  const Img = styled("img")({
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  });
   return (
     <div>
+      {/* This is where the techniques are inserted into the page */}
       {currTechniqueList.map((technique) => (
-        <div>
-          <h1>{technique.title}</h1>
-          <p>{technique.description}</p>
-        </div>
+        <Grid container alignItems={"center"} justifyContent={"center"}>
+          <Grid container xs={8} className={`${styles.techniqueContainer}`}>
+            <Grid item sx={{ width: 128, height: 128, p: 1 }}>
+              <Img src={technique.image} alt="placeholder" />
+            </Grid>
+            <Grid>
+              <Grid container xs={12} sx={{ p: 1 }} justifyItems={"left"}>
+                <h1>{technique.title}</h1>
+              </Grid>
+              <Grid container sx={{ p: 1 }}>
+                <p>{technique.description}</p>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       ))}
     </div>
   );
