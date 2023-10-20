a55152aa7b8bed912b82ff760981f4c5c3dd6e69
William Brown <willis291@gmail.com>
2023-10-20 11:49:12 -0600

    small change to note

--- client/src/assets/Techniques.js
+++ client/src/assets/Techniques.js
@@ -2,7 +2,7 @@
 // Each of the guidelines is defined here as a key within the Techniques object. Each key contains an array of objects that contain the info that shall be rendered on the guideline page
 import { Placeholder, Red } from "../assets/images/techniques";
 const Techniques = {
-  //NOTE: A good way of doing the text of the article could just be to have the text aspects of each article imported as variables, and the "description" here could just render a certain number of the characters from the variable followed by a "Read More" button that would take the user to the full article, similar to the way that the current live website displays
+  //NOTE: A good way of doing the text of the article could just be to have the sub-headline text of each article imported as variables, and the "description" here could just render a certain number of the characters from the variable followed by a "Read More" button that would take the user to the full article, similar to the way that the current live website displays
   guideline1Techniques: [
     {
       title: "Integrate ‘Writing for Visual Narrative’",
